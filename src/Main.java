32ccbb386e6a688c0fa5b20418e7ac2b02fb0569
Stav Iatrop <staviatrop@gmail.com>
2020-03-22 18:15:00 +0200

    Added infinite while loop to Main. Infinite loop when I enter ctrl+D.

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,20 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            CalcEvaluator evaluate = new CalcEvaluator(System.in);
-            System.out.println(evaluate.eval());
+
+        while (true) {
+            try {
+                CalcEvaluator evaluate = new CalcEvaluator(System.in);
+                System.out.println(evaluate.eval());
+            }
+            catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+            catch(ParseError err){
+                System.err.println(err.getMessage());
+            }
         }
-        catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-        catch(ParseError err){
-            System.err.println(err.getMessage());
-        }
+
         
     }
 }
