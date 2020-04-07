52cf1e1880e66cc1e0de5bf6a6489fe2a3650694
Stav Iatrop <staviatrop@gmail.com>
2020-04-07 23:26:53 +0300

    added minijava grammar file and Main file

---
+++ Main.java
@@ -0,0 +1,38 @@
+import syntaxtree.*;
+import visitor.*;
+import java.io.*;
+
+public class Main {
+    public static void main (String [] args){
+    if(args.length != 1){
+        System.err.println("Usage: java Main <inputFile>");
+        System.exit(1);
+    }
+    FileInputStream fis = null;
+    try{
+        fis = new FileInputStream(args[0]);
+        //following block may need change
+        ///////////////////////////////////////////////
+        MiniJavaParser parser = new MiniJavaParser(fis);
+        Goal root = parser.Goal();
+        System.err.println("Program parsed successfully.");
+        EvalVisitor eval = new EvalVisitor();
+        System.out.println(root.accept(eval, null));
+        ///////////////////////////////////////////////
+    }
+    catch(ParseException ex){
+        System.out.println(ex.getMessage());
+    }
+    catch(FileNotFoundException ex){
+        System.err.println(ex.getMessage());
+    }
+    finally{
+        try{
+        if(fis != null) fis.close();
+        }
+        catch(IOException ex){
+        System.err.println(ex.getMessage());
+        }
+    }
+    }
+}
