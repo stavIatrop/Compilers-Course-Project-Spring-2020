37bafcdbd4722ecf84ce70672b18ddf9c1ef9517
Stav Iatrop <staviatrop@gmail.com>
2020-04-17 14:05:27 +0300

    addition of class declaration with its fields and methods in symbol table completed

--- FirstPhaseVisitor.java
+++ FirstPhaseVisitor.java
@@ -4,10 +4,54 @@ import parse_error.*;
 
 public class FirstPhaseVisitor extends GJDepthFirst<String, SymbolTable> {
 
-   String currentClass;
-   boolean classVar;
-   String currentMethod;
+   String currentClass = null;
+   boolean classVar = false;
+   String currentMethod = null;
 
+
+      /**
+    * f0 -> "class"
+    * f1 -> Identifier()
+    * f2 -> "{"
+    * f3 -> "public"
+    * f4 -> "static"
+    * f5 -> "void"
+    * f6 -> "main"
+    * f7 -> "("
+    * f8 -> "String"
+    * f9 -> "["
+    * f10 -> "]"
+    * f11 -> Identifier()
+    * f12 -> ")"
+    * f13 -> "{"
+    * f14 -> ( VarDeclaration() )*
+    * f15 -> ( Statement() )*
+    * f16 -> "}"
+    * f17 -> "}"
+    */
+    public String visit(MainClass n, SymbolTable sTable) throws ParseError{
+      String _ret=null;
+      this.currentClass = "main";
+      n.f0.accept(this, sTable);
+      n.f1.accept(this, sTable);
+      n.f2.accept(this, sTable);
+      n.f3.accept(this, sTable);
+      n.f4.accept(this, sTable);
+      n.f5.accept(this, sTable);
+      n.f6.accept(this, sTable);
+      n.f7.accept(this, sTable);
+      n.f8.accept(this, sTable);
+      n.f9.accept(this, sTable);
+      n.f10.accept(this, sTable);
+      n.f11.accept(this, sTable);
+      n.f12.accept(this, sTable);
+      n.f13.accept(this, sTable);
+      n.f14.accept(this, sTable);
+      n.f15.accept(this, sTable);
+      n.f16.accept(this, sTable);
+      n.f17.accept(this, sTable);
+      return _ret;
+   }
 
 
     /**
@@ -60,11 +104,18 @@ public class FirstPhaseVisitor extends GJDepthFirst<String, SymbolTable> {
       return_type = n.f1.accept(this, sTable);
       funName = n.f2.accept(this, sTable);
       this.currentMethod = funName;
+
+      ClassInfo cinfo = sTable.hmap.get(this.currentClass);
+      if (cinfo.class_methods.containsKey(funName)) {
+         throw new ParseError("Function name " + funName + " already declared in class " + this.currentClass);
+      }
+      cinfo.class_methods.put(funName, new FunInfo(return_type, false));          //Needs extra code for virtual
+
       n.f3.accept(this, sTable);
-      //Stopped HERE!
       n.f4.accept(this, sTable);
       n.f5.accept(this, sTable);
       n.f6.accept(this, sTable);
+      this.classVar = false;
       n.f7.accept(this, sTable);
       n.f8.accept(this, sTable);
       n.f9.accept(this, sTable);
@@ -79,9 +130,10 @@ public class FirstPhaseVisitor extends GJDepthFirst<String, SymbolTable> {
     * f1 -> FormalParameterTail()
     */
     public String visit(FormalParameterList n, SymbolTable sTable) throws ParseError{
-      String _ret=null;
-      _ret = n.f0.accept(this, sTable);
-      _ret = _ret + n.f1.accept(this, sTable);
+      String _ret = null;
+      String param;
+      param = n.f0.accept(this, sTable);
+      _ret = param + n.f1.accept(this, sTable);
       return _ret;
    }
 
@@ -93,6 +145,15 @@ public class FirstPhaseVisitor extends GJDepthFirst<String, SymbolTable> {
       String type, id;
       type = n.f0.accept(this, sTable);
       id = n.f1.accept(this, sTable);
+      if (this.currentClass != "main") {
+         ClassInfo cinfo = sTable.hmap.get(this.currentClass);
+         FunInfo finfo = cinfo.class_methods.get(this.currentMethod);
+         if (finfo.arg_types.containsKey(id)) {
+            throw new ParseError("Duplicate parameter " + id + " in function " + this.currentMethod);
+         }
+         finfo.arg_types.put(id, new VarInfo(type, null));
+      }
+      
       return type + " " + id;
    }
 
@@ -112,32 +173,31 @@ public class FirstPhaseVisitor extends GJDepthFirst<String, SymbolTable> {
     * f1 -> Identifier()
     * f2 -> ";"
     */
-   // public String visit(VarDeclaration n, SymbolTable sTable) throws ParseError {
-   //    String _ret=null;
-   //    String type, id;
-   //    type = n.f0.accept(this, sTable);
-   //    id = n.f1.accept(this, sTable);
-   //    ClassInfo cinfo = sTable.hmap.get(this.currentClass);
-   //    if (classVar) {         //If variable declared is a class field
+   public String visit(VarDeclaration n, SymbolTable sTable) throws ParseError {
+      String type, id;
+      type = n.f0.accept(this, sTable);
+      id = n.f1.accept(this, sTable);
+      ClassInfo cinfo = sTable.hmap.get(this.currentClass);
+      if (classVar) {         //If variable declared is a class field
          
-   //       if (cinfo.class_vars.containsKey(id)) {		//field with the same name already declared
-   //          throw new ParseError("Field with name " + id + " in class " + this.currentClass + " already declared");
-   //       }
-   //       cinfo.class_vars.put(id, new VarInfo(type, null));
-
-   //    }
-   //    //if variable declared is a method variable
-   //    FunInfo finfo = cinfo.class_methods.get(this.currentMethod);
-   //    if (finfo.fun_vars.containsKey(id)) {        //variable with the same name already declared
-   //       throw new ParseError("Duplicate local variable  " + id + " in function " + this.currentMethod);
+         if (cinfo.class_vars.containsKey(id)) {		//field with the same name already declared
+            throw new ParseError("Field with name " + id + " in class " + this.currentClass + " already declared");
+         }
+         cinfo.class_vars.put(id, new VarInfo(type, null));
+         return type + " " + id;
+      }
+      //if variable declared is a method variable
+      FunInfo finfo = cinfo.class_methods.get(this.currentMethod);
+      if ( finfo.fun_vars.containsKey(id)) {        //variable with the same name already declared
+         throw new ParseError("Duplicate local variable  " + id + " in function " + this.currentMethod);
          
-   //    } else if (finfo.arg_types.containsKey(id)) {
-   //       throw new ParseError("Local variable and function parameter have the same name " + id + " in function " + this.currentMethod);
-   //    }
-   //    finfo.fun_vars.put(id, new VarInfo(type, null));
-   //    n.f2.accept(this, sTable);
-   //    return _ret;
-   // }
+      } else if (finfo.arg_types.containsKey(id)) {
+         throw new ParseError("Local variable and function parameter have the same name " + id + " in function " + this.currentMethod);
+      }
+      finfo.fun_vars.put(id, new VarInfo(type, null));
+      n.f2.accept(this, sTable);
+      return type + " " + id;
+   }
 
    public String visit(NodeToken n, SymbolTable sTable) {    return n.toString(); }
 
